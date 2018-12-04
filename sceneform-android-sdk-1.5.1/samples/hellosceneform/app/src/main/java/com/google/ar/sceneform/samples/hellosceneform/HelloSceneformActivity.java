b098ceb0f97fcb59449702462fc1eef9bf9d132e
ggadek <34471121+Grzeogrz@users.noreply.github.com>
2018-12-04 22:21:02 +0100

    loading image
    load of last screenshot added

--- sceneform-android-sdk-1.5.1/samples/hellosceneform/app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/HelloSceneformActivity.java
+++ sceneform-android-sdk-1.5.1/samples/hellosceneform/app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/HelloSceneformActivity.java
@@ -15,15 +15,28 @@
  */
 package com.google.ar.sceneform.samples.hellosceneform;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
@@ -46,7 +59,11 @@ import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -67,7 +84,11 @@ public class HelloSceneformActivity extends AppCompatActivity implements View.On
   private Button screenShotButton = null;
   private ConstraintLayout rootContent = null;
 
+  public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+
   ImageView imageToDisplay = null;
+
+    private int PICK_IMAGE_REQUEST = 1;
 
   @Override
   @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
@@ -86,7 +107,7 @@ public class HelloSceneformActivity extends AppCompatActivity implements View.On
     // When you build a Renderable, Sceneform loads its resources in the background while returning
     // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
     ModelRenderable.builder()
-        .setSource(this, R.raw.model)//R.raw.andy)
+        .setSource(this, R.raw.andy)//R.raw.andy)
         .build()
         .thenAccept(renderable -> andyRenderable = renderable)
         .exceptionally(
@@ -135,12 +156,13 @@ public class HelloSceneformActivity extends AppCompatActivity implements View.On
             imageToDisplay.setAlpha(0.5f);
             imageToDisplay.setVisibility(View.INVISIBLE);
             imageToDisplay.setImageResource(R.drawable.screen);
+            //takeImage();
 
 //            takeScreenShot();
 
           // Create the transformable andy and add it to the anchor.
           TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
-          andy.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 0, 1f), 90f));
+          //andy.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 0, 1f), 90f));
           andy.setParent(anchorNode);
           andy.setRenderable(andyRenderable);
           andy.select();
@@ -185,6 +207,8 @@ public class HelloSceneformActivity extends AppCompatActivity implements View.On
   @Override
   public void onClick(View v) {
       if (v.getId() == R.id.screenshot && imageToDisplay.getVisibility() == View.INVISIBLE){
+          takeScreenShot();
+          takeImage(v.getContext());
           imageToDisplay.setVisibility(View.VISIBLE);
       }
       else
@@ -194,16 +218,146 @@ public class HelloSceneformActivity extends AppCompatActivity implements View.On
   }
 
   private void takeScreenShot(){
-      Bitmap bitmap = null;
-      //screenShotButton = findViewById(R.id.screenshot);
-      //screenShotButton.setOnClickListener(this);
-      rootContent = (ConstraintLayout) findViewById(R.id.rootContent);
-      bitmap = ScreenshotUtils.getScreenShot(rootContent);
+      Date now = new Date();
+      android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
 
-      if (bitmap != null){
-          File savedFile = ScreenshotUtils.getMainDirectoryName(this);
-          File file = ScreenshotUtils.storeScreenShot(bitmap, "screenshot.jpg", savedFile);
+      try {
+          // image naming and path  to include sd card  appending name you choose for file
+          String mPath = Environment.getRootDirectory().toString() + "/" + now + ".jpg";
+
+          // create bitmap screen capture
+          View v1 = getWindow().getDecorView().getRootView();
+          v1.setDrawingCacheEnabled(true);
+          Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+          v1.setDrawingCacheEnabled(false);
+
+          File imageFile = new File(mPath);
+
+          FileOutputStream outputStream = new FileOutputStream(imageFile);
+          int quality = 100;
+          bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+          outputStream.flush();
+          outputStream.close();
+
+          openScreenshot(imageFile);
+      } catch (Throwable e) {
+          // Several error may come out with file handling or DOM
+          e.printStackTrace();
       }
   }
 
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
+    }
+
+  private void takeImage(Context context) {
+//      Intent intent = new Intent();
+//      intent.setType("image/*");
+//      intent.setAction(Intent.ACTION_GET_CONTENT);
+//      startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+      if (checkPermissionREAD_EXTERNAL_STORAGE(context)) {
+          String[] projection = new String[]{
+                  MediaStore.Images.ImageColumns._ID,
+                  MediaStore.Images.ImageColumns.DATA,
+                  MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
+                  MediaStore.Images.ImageColumns.DATE_TAKEN,
+                  MediaStore.Images.ImageColumns.MIME_TYPE
+          };
+          ContentResolver contentResolver = context.getContentResolver();
+          Cursor cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null,
+                  null, MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC");
+
+          if (cursor.moveToFirst()) {
+              final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+              String imageLocation = cursor.getString(1);
+              File imageFile = new File(imageLocation);
+              if (imageFile.exists()) {   // TODO: is there a better way to do this?
+                  Bitmap bm = BitmapFactory.decodeFile(imageLocation);
+                  imageView.setImageBitmap(bm);
+              }
+          }
+      }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            Uri uri = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                ImageView imageView = findViewById(R.id.imageView);
+                imageView.setImageBitmap(bitmap);
+            }catch (IOException exception){
+                exception.printStackTrace();
+            }
+        }
+  }
+
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE(final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context,
+                            Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // do your stuff
+                } else {
+                    Toast.makeText(getBaseContext(), "GET_ACCOUNTS Denied",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions,
+                        grantResults);
+        }
+    }
 }
